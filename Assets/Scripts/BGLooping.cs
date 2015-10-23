a6772221e51c1f903aefae91836242596b1595aa
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2015-10-23 20:25:51 +0200

    Delete offscreen objects

--- Assets/Scripts/BGLooping.cs
+++ Assets/Scripts/BGLooping.cs
@@ -5,6 +5,10 @@ public class BGLooping : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collider) {
 
+		if (collider.tag == "Cloud") {
+			Destroy(collider.gameObject);
+		}
+
 		float heigthOfBG = ((BoxCollider2D)collider).size.y;
 		Vector3 pos = collider.transform.position;
 		pos.y += heigthOfBG * 4;
