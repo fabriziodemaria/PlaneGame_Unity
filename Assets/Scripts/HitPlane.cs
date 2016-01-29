034d371460efaee12edb19295461cf9c2d3b224d
fabriziodemaria <fabrizio.f.demaria@gmail.com>
2016-01-29 22:41:43 +0100

    Push of previously untracked files
    Reduced godMode timespan

---
+++ Assets/Scripts/HitPlane.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPlane : MonoBehaviour {
+
+	public float blinkRate;
+	public float blinkTime;
+	private bool active = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void StartGodMode () {
+		if (active) return;
+		StartCoroutine(BlinkingPlane(blinkTime));
+		this.gameObject.GetComponent<Renderer>().enabled = false;
+		this.GetComponent<PlaneMovement>().godMode = true;
+		active = true;
+	}
+
+
+	IEnumerator BlinkingPlane (float blinkingTime) {
+		float endtime = Time.time + blinkingTime;
+		while (Time.time < endtime) {
+			this.gameObject.GetComponent<Renderer>().enabled = false;
+			yield return new WaitForSeconds(blinkRate);
+			this.gameObject.GetComponent<Renderer>().enabled = true;
+			yield return new WaitForSeconds(blinkRate);
+		}
+		this.GetComponent<PlaneMovement>().godMode = false;
+		active = false;
+	}
+}
